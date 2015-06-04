bd3598b0e7c3ecd56abb7502841ce2eba53cc831
nhawa <nhawa.c.ec@gmail.com>
2015-06-04 23:09:27 +0800

    first commit

---
+++ src/laporan/pkg2/pbo/Laporan2PBO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laporan.pkg2.pbo;
+
+/**
+ *
+ * @author nhawa
+ */
+public class Laporan2PBO {
+    static boolean bool;
+    static int nilai1;
+    static long nilai2;
+    static short nilai3;
+    static String \u0073\u0074\u0072 = "Laporan 2 PBO";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        if (str != null) 
+        {
+            Boolean refBool = true;
+            boolean bool1 = refBool;
+            System.out.println("bool " + bool1);
+            
+            Integer refInt = 24000;
+            int i = refInt;
+            System.out.println("int " + i);
+            
+            Long refL = (long) 602001120;
+            long l = refL;
+            System.out.println("long " + l);
+            
+            Short refSh = (short) 'Z';
+            short s = refSh;
+            System.out.println("short " + s);
+            
+            System.out.println(str);
+            
+        }
+        else {
+            System.out.println("nilai default boolean " + bool);
+            System.out.println("nilai default int " + nilai1);
+			System.out.println("nilai default long " + nilai2);
+			System.out.println("nilai default short " + nilai3);
+			System.out.println(str);
+			
+		}		
+    }
+    
+}
